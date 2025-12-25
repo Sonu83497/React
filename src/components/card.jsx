877f332561a54a18cb86c29666890211ce14f64c
Sonu Prajapati <sonuprajapati83497@gmail.com>
2025-12-25 08:02:32 +0530

    initial commit

---
+++ src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card  =() =>{
+    return (
+        <div>
+        <div className='card'>
+            <h1>Sonu Prajapati</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nisi veniam molestiae perferendis praesentium ipsam voluptates, repellendus cum delectus corporis rerum libero magni nobis! Sit ipsum quasi libero facere ullam?</p>
+        </div>
+        </div>
+    )
+}
+
+export default Card
